08e46d8e6343b270bab3a939cd0762c76430e331
Ray <xcz1207@gmail.com>
2024-02-27 13:06:05 -0800

    added a test version

---
+++ 8.3+Travel+Tracker/8.3 Travel Tracker/testIndex.js
@@ -0,0 +1,68 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import pg from 'pg';
+const app = express();
+const port = 3000;
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended:true}));
+
+const db = new pg.Client({
+    user: "postgres",
+    host: "localhost",
+    database: "world",
+    password: "Jia3202128",
+    port: 5432,
+});
+db.connect();
+//checkVisited()
+async function checkVisited(){
+    let result = db.query("SELECT countries_code FROM visited_countries");
+    result = result.rows;
+    let newArr;
+    result.forEach(country =>{
+        newArr.push(country.countries_code);
+    })
+    return newArr;
+}
+//get '/'
+app.get("/", async(req, res)=>{
+    let result = await checkVisited();
+    res.render("index.ejs",{
+        total: result.length,
+        countries: result,
+    })
+})
+//post add
+app.post("/add", async(req, res)=>{
+    let usersubmit = req.body.country;
+    try {
+        let result = await db.query("SELECT countries_code FROM countries WHERE LOWER(countries_name) LIKE '%'||$1||'%');", [usersubmit].toLowerCase());
+        result = result.rows[0].countries_code;
+        try {
+            let insertResult = db.query("INSERT INTO visited_countries(country_code) VALUES($1);", [result]);
+            let data = checkVisited();
+            res.render("index.ejs", {
+                total:data.length,
+                countries: data,
+            })
+        } catch (error) {
+            let data = checkVisited();
+            res.render("index.ejs",{
+                total: data.length,
+                countries: data,
+                error: "Country already added."
+            })
+        }
+    } catch (error) {
+        let data = checkVisited();
+        res.render("index.ejs", {
+            total: data.length,
+            countries: data,
+            error: "Cant't find the country you entered. Please try agian",
+        })
+    }
+})
+//listen
+app.listen(port, (req, res)=>{
+    console.log(`Server on port ${port}`);
+})
